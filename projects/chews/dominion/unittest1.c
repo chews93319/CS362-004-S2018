e4432404eb2b5dac80542620bc46b782d2ea38d5
chews93319 <chews@oregonstate.edu>
2018-04-24 12:45:59 -0700

    Prepared Makefile and first unittest for coding and compiling

---
+++ projects/chews/dominion/unittest1.c
@@ -0,0 +1,36 @@
+
+/* 
+ * File:   unittest1.c
+ * Author: Sam Chew
+ * ONID: chews 933191613
+ *
+ * Created on April 24, 2018, 12:23 PM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <assert.h>
+#include "dominion.h"
+#include "dominion_helpers.h"
+#include "rngs.h"
+
+
+//set NOISY1 to 0 to remove printfs from output
+#define NOISY1 1
+
+
+/*
+ * Unit Test for Function: playCard
+ */
+int main(int argc, char** argv) {
+    
+#if (NOISY1 == 1)
+    printf("*** This was Unit Test 1 ***\n");
+#endif
+    
+    
+    
+    return (EXIT_SUCCESS);
+}
+
